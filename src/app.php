b89cfc3200f9684e82370fda8a07846af22acbe2
Isaac Leinweber <brotchen@gmail.com>
2015-09-10 22:36:10 +0000

    Add AWS SDK as a dependency.  Include Silex provider
    Also wire it up.

--- src/app.php
+++ src/app.php
@@ -1,16 +1,27 @@
 <?php
 
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
+use Aws\Silex\AwsServiceProvider;
 
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
+
+$app->register(new AwsServiceProvider(), [
+    'aws.config' => [
+        'version' => 'latest',
+        'region' => 'us-west-2',
+    ]
+]);
+
 $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
